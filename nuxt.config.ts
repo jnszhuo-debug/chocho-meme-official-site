8f4cff1cba55f15c110ce2abbb25c7a25e4b1d5b
Jonathan <jns.cho@JnsMB.local>
2026-04-26 11:12:23 +0700

    fix: force static preset to avoid netlify-legacy ENOENT error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [],
+  nitro: {
+    preset: 'static',
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
